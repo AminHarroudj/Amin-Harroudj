4e538f284259581755085f9a839e4209aa837dad
AminHarroudj <amin.harroudj.2002@gmail.com>
2024-10-22 11:37:23 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import img1 from "./images/img1.png";
 import mawaqitImg from "./images/mawaqit.png";
 import dentistImg from "./images/dentiste.png";
 import aifaImg from "./images/aifa.png";
+import clubbasmaImg from "./images/basmaclub.png";
+import portfolioImg from "./images/portfolio.png";
+import todolistImg from "./images/todolist.png";
 import backGround from "./images/bg1.png";
 import { FaEnvelope } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
@@ -16,7 +19,13 @@ import { FaJava } from "react-icons/fa";
 import { FaNode } from "react-icons/fa";
 import { FaReact } from "react-icons/fa";
 import { FaBars } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { SiFramer } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { TbBrandMysql } from "react-icons/tb";
 
 function App() {
   return (
@@ -29,6 +38,7 @@ function App() {
       <NavBar />
       <HomePage />
       <AboutMe />
+      <LearningPath />
       <MyProjects />
       <ContactMe />
       <Footer />
@@ -42,7 +52,9 @@ function NavBar() {
     <div className="flex justify-around items-center py-6 shadow-md fixed top-0 left-0 right-0 bg-white opacity-90 z-10 max-sm:justify-between max-sm:px-6">
       <h1 className="text-3xl font-bold text-sky-500">Amin</h1>
       <ul
-        className={`flex gap-10 text-xl font-medium max-sm:flex-col max-sm:bg-sky-500 max-sm:absolute max-sm:top-20 max-sm:right-0 max-sm:left-20 max-sm:text-center max-sm:py-4 max-sm:pl-10 max-sm:text-white max-sm:rounded-tl-full max-sm:rounded-bl-full ${open ? 'max-sm:flex':'max-sm:hidden'}`}
+        className={`flex gap-10 text-xl font-medium max-sm:flex-col max-sm:bg-sky-500 max-sm:absolute max-sm:top-20 max-sm:right-0 max-sm:left-20 max-sm:text-center max-sm:py-4 max-sm:pl-10 max-sm:text-white max-sm:rounded-tl-full max-sm:rounded-bl-full ${
+          open ? "max-sm:flex" : "max-sm:hidden"
+        }`}
         id="menu"
       >
         <li className="hover:text-sky-500 hover:duration-200">
@@ -52,14 +64,20 @@ function NavBar() {
           <a href="#about">About</a>
         </li>
         <li className="hover:text-sky-500 hover:duration-200">
+          <a href="#about">Learning</a>
+        </li>
+        <li className="hover:text-sky-500 hover:duration-200">
           <a href="#projects">Projects</a>
         </li>
         <li className="hover:text-sky-500 hover:duration-200">
           <a href="#contact">Contact</a>
         </li>
       </ul>
-      <div onClick={()=>setOpen(!open)} className="text-2xl hidden max-sm:flex cursor-pointer">
-        <FaBars id="menu-btn"/>
+      <div
+        onClick={() => setOpen(!open)}
+        className="text-2xl hidden max-sm:flex cursor-pointer"
+      >
+        <FaBars id="menu-btn" />
       </div>
     </div>
   );
@@ -81,30 +99,38 @@ function HomePage() {
       <div className="flex flex-col gap-4 text-4xl font-semibold max-sm:items-center max-sm:justify-center max-sm:text-center">
         <h1 className="text-sky-500">Hello Friends</h1>
         <h1>I'm Mohammed El Amin</h1>
-        <p>Web Developper</p>
+        <p>Web Developer</p>
         <Button title={"Download CV"} />
         <div className="flex gap-4">
           <a
-            href="www.linkedin.com"
+            href="https://www.linkedin.com/in/harroudj-mohammed-el-amin-655315252/"
             className="hover:text-sky-500 hover:duration-300"
+            target="_blank"
+            rel="noreferrer"
           >
             <FaLinkedin />
           </a>
           <a
-            href="www.github.com"
+            href="https://github.com/AminHarroudj"
             className="hover:text-sky-500 hover:duration-300"
+            target="_blank"
+            rel="noreferrer"
           >
             <FaGithub />
           </a>
           <a
-            href="www.facebook.com"
+            href="https://web.facebook.com/amin.harroudj.1"
             className="hover:text-sky-500 hover:duration-300"
+            target="_blank"
+            rel="noreferrer"
           >
             <FaFacebook />
           </a>
           <a
-            href="www.instagram.com"
+            href="https://www.instagram.com/mohammedelaminharroudj/"
             className="hover:text-sky-500 hover:duration-300"
+            target="_blank"
+            rel="noreferrer"
           >
             <FaInstagram />
           </a>
@@ -137,18 +163,18 @@ function AboutMe() {
         Web Developper Full Stack{" "}
         <span className="text-sky-500">MERN Stack</span>
       </p>
-      <div>
-        <CarteSkill title="JavaScript" description="Advance Level">
-          <FaJsSquare />
+      <div className="mt-10 flex flex-col gap-4 lg:flex-row">
+        <CarteSkill title="Mongo DB" description="Intermidiate Level">
+          <SiMongodb />
+        </CarteSkill>
+        <CarteSkill title="Express Js" description="Advanced Level">
+          <SiExpress />
         </CarteSkill>
         <CarteSkill title="React JS" description="Intermidiate Level">
           <FaReact />
         </CarteSkill>
         <CarteSkill title="Node JS" description="Advance Level">
           <FaNode />
-        </CarteSkill>
-        <CarteSkill title="Java" description="Beginner Level">
-          <FaJava />
         </CarteSkill>
       </div>
     </div>
@@ -157,10 +183,58 @@ function AboutMe() {
 
 function CarteSkill({ children, title = "null", description = "null" }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 bg-gray-100 py-6 px-64 mt-10 rounded-xl max-sm:px-20">
+    <div className="flex flex-col justify-center items-center gap-4 bg-gray-100 py-6 px-16 rounded-xl max-sm:px-20 hover:bg-gray-200 cursor-pointer">
       <span className="text-8xl text-sky-500">{children}</span>
       <h1 className="text-xl font-semibold">{title}</h1>
       <p className="text-lg text-gray-600">{description}</p>
+    </div>
+  );
+}
+
+function LearningPath() {
+  return (
+    <div
+      className="flex flex-col justify-center items-center pt-24 gap-4 max-sm:text-center"
+      id="learningpath"
+    >
+      <h1 className="text-sky-500 text-5xl font-semibold max-sm:text-4xl">
+        My Learning Path
+      </h1>
+      <p className="text-2xl font-medium text-gray-600">
+        Here I will show you my path of learning website development
+      </p>
+      <div className="grid grid-cols-1 mt-10 lg:grid-cols-4 gap-4">
+        <CarteSkill title="HTML" description="Advanced Level">
+          <FaHtml5 />
+        </CarteSkill>
+        <CarteSkill title="CSS" description="Advanced Level">
+          <FaCss3Alt />
+        </CarteSkill>
+        <CarteSkill title="Java Script" description="Advanced Level">
+          <FaJsSquare />
+        </CarteSkill>
+        <CarteSkill title="React JS" description="Advanced Level">
+          <FaReact />
+        </CarteSkill>
+        <CarteSkill title="Framer Motion" description="Advanced Level">
+          <SiFramer />
+        </CarteSkill>
+        <CarteSkill title="Node JS" description="Advanced Level">
+          <FaNode />
+        </CarteSkill>
+        <CarteSkill title="Express Js" description="Advanced Level">
+          <SiExpress />
+        </CarteSkill>
+        <CarteSkill title="Mongo DB" description="Intermidiate Level">
+          <SiMongodb />
+        </CarteSkill>
+        <CarteSkill title="My SQL" description="Intermidiate Level">
+          <TbBrandMysql />
+        </CarteSkill>
+        <CarteSkill title="JAVA" description="Beginner Level">
+          <FaJava />
+        </CarteSkill>
+      </div>
     </div>
   );
 }
@@ -171,22 +245,60 @@ function MyProjects() {
       className="flex flex-col justify-center items-center gap-4 pt-24"
       id="projects"
     >
-      <h1 className="text-sky-500 text-5xl font-semibold">My Project</h1>
+      <h1 className="text-sky-500 text-5xl font-semibold">My Projects</h1>
       <p className="text-2xl font-medium text-gray-600">My awesome works</p>
-      <div className="grid grid-cols-2 gap-10 max-sm:grid-cols-1">
-        <ProjectCard imageSrc={mawaqitImg} title="Mawaqit Salat" />
-        <ProjectCard imageSrc={dentistImg} title="Dentist Website" />
-        <ProjectCard imageSrc={aifaImg} title="Aifa Hotel" />
-        <ProjectCard imageSrc={mawaqitImg} title="Mawaqit Salat" />
+      <div className="grid grid-cols-3 mt-10 gap-10 max-sm:grid-cols-1">
+        <a
+          href="https://amin-harroudj.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ProjectCard imageSrc={portfolioImg} title="Portfolio" />
+        </a>
+        <a
+          href="https://club-basma.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ProjectCard imageSrc={clubbasmaImg} title="Club Basma" />
+        </a>
+        <a
+          href="https://aminharroudj.github.io/AIFA_Hotel/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ProjectCard imageSrc={aifaImg} title="Aifa Hotel" />
+        </a>
+        <a
+          href="https://aminharroudj.github.io/dentist-website/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ProjectCard imageSrc={dentistImg} title="Dentist Website" />
+        </a>
+        <a
+          href="https://todo-list-theta-seven-32.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ProjectCard imageSrc={todolistImg} title="To Do List" />
+        </a>
+        <a
+          href="https://mawaqit-salat-react.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ProjectCard imageSrc={mawaqitImg} title="Mawaqit Salat Algeria" />
+        </a>
       </div>
     </div>
   );
 }
 
-function ProjectCard({ imageSrc, title = "null" }) {
+function ProjectCard({ children, imageSrc, title = "null" }) {
   return (
-    <div className="w-96 pb-4 flex flex-col gap-4 items-center border-4 rounded-xl max-sm:w-72">
-      <img src={imageSrc} alt={title} className="w-96 rounded-xl" />
+    <div className="cursor-pointer w-96 pb-4 flex flex-col gap-4 items-center border-2 rounded-lg max-sm:w-72 hover:bg-gray-100">
+      <img src={imageSrc} alt={title} className="w-96 rounded-lg" />
       <h1 className="text-xl font-medium">{title}</h1>
     </div>
   );
@@ -226,7 +338,7 @@ function ContactMe() {
             <div className="bg-sky-500 text-white text-2xl rounded-full p-4">
               <FaEnvelope />
             </div>
-            <p>amin@gmail.com</p>
+            <p>amin.harroudj.2002@outlook.com</p>
           </div>
           <div className="flex gap-4 items-center">
             <div className="bg-sky-500 text-white text-2xl rounded-full p-4">
